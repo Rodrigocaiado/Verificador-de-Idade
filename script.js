74ace209ba6cad15d2d2edf49ac226648796db1c
Rodrigo Yvan Caiado <106771261+Rodrigocaiado@users.noreply.github.com>
2022-10-04 17:29:14 -0300

    incluindo fotos

--- script.js
+++ script.js
@@ -13,19 +13,37 @@ function verificar(){
         img.setAttribute('id', 'foto')
         if(sex[0].checked){
             genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                // Criança
+                img.setAttribute('src', 'kidsmulher.png')
+            } else if (idade < 20){
+                // Jovem
+                img.setAttribute('src', 'adolescentmulher.png')
+            } else if (idade < 50){
+                // Adulto
+                img.setAttribute('src', 'mulher.png')
+            } else{
+                // Idoso
+                img.setAttribute('src', 'idosa.png')
+            }
         } else if (sex[1].checked){
             genero = 'Homem'
             if(idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'kidshomem.png' )
             } else if(idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'adolescenthomem.png')
             }else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'homem.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'idoso.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
